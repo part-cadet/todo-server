f62805829a992af3b0b422f33ebcd84b87927ae4
Galiold <ali.goldani@ymail.com>
2019-07-15 17:12:41 +0430

    Script updated

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ var i, count = 0;
 
 window.onload = () => { 
     var boardBoxWidth = document.getElementById("boardBox").clientWidth;
-    // console.log("Board Box Width = " + boardBoxWidth);
+    console.log("Board Box Width = " + boardBoxWidth);
     var boards = document.getElementsByClassName("board");
-    // console.log("Board Count = " + boards.length);
-    // console.log("Board Width = " + boards[0].clientWidth);
+    console.log("Board Count = " + boards.length);
+    console.log("Board Width = " + boards[0].clientWidth);
     var columns = (Math.floor(boardBoxWidth /  (boards[0].clientWidth + 20)));
-    var emptyBoardsNeeded = columns - (boards.length % columns);
+    console.log("Columns = " + columns);
+    var emptyBoardsNeeded = (boards.length % columns) === 0 ? 0 : columns - (boards.length % columns);
 
-    // console.log("empty needed = " + emptyBoardsNeeded);
+    console.log("empty needed = " + emptyBoardsNeeded);
     for (i = 0; i < emptyBoardsNeeded; i++) {
         var emptyDiv = document.createElement("DIV");
         emptyDiv.setAttribute("class", "empty");
